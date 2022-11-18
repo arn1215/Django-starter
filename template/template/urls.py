6d0690da66f7ea6deaafd520b7002babc8fa916b
Ali <ali.naqvi.dev@gmail.com>
2022-11-18 15:19:05 -0600

    removed extra folders

--- template/template/urls.py
+++ template/template/urls.py
@@ -1,4 +1,4 @@
-"""djan URL Configuration
+"""template URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
