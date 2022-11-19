de4b9a6e8fc0069047cac42da16087e7db0b24c2
Ali <ali.naqvi.dev@gmail.com>
2022-11-18 18:47:57 -0600

    added app views

--- template/base/views.py
+++ template/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(req):
+    return HttpResponse("Welcome Home.")
+
+def shop(req):
+    return HttpResponse("Shop All Candles")
