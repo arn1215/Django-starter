251730904a1438810b09a518101ee8e3cc59f198
Ali <ali.naqvi.dev@gmail.com>
2022-11-30 14:21:23 -0600

    home template changed

--- template/base/views.py
+++ template/base/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -8,7 +8,10 @@ from .forms import RoomForm
 
 def home(req):
     rooms = Room.objects.all()
-    context = {'rooms': rooms}
+
+    topics = Topic.objects.all()
+
+    context = {'rooms': rooms, 'topics': topics}
     return render(req, 'base/home.html', context)
 
 def room(req, pk):
@@ -18,5 +21,32 @@ def room(req, pk):
 
 def createRoom(req):
   form = RoomForm()
+  if req.method == 'POST':
+    form = RoomForm(req.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+
+    print(req.POST)
   context = {'form': form}
   return render(req, 'base/room_form.html', context)
+
+def updateRoom(req, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if req.method == 'POST':
+      form = RoomForm(req.POST, instance=room)
+      if form.is_valid():
+        form.save()
+        return redirect('home')
+    
+    context = {'form': form}
+    return render(req, 'base/room_form.html', context)
+
+def deleteRoom(req, pk):
+  room = Room.objects.get(id=pk)
+  if req.method == "POST":
+    room.delete()
+    return redirect('home')
+  return render(req, 'base/delete.html', {'obj': room})
