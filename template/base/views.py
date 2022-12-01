f3ccfb7add079ac2c2509ba55fdc956bc562f946
Ali <ali.naqvi.dev@gmail.com>
2022-12-01 01:51:48 -0600

    basic auth form connected

--- template/base/views.py
+++ template/base/views.py
@@ -2,18 +2,56 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
-
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def loginPage(req): 
+
+  if req.method == 'POST':
+    username = req.POST.get('username')
+    password = req.POST.get('password')
+    print(username, password)
+
+    try:
+      user = User.objects.get(username=username)
+    except:
+      messages.error(req, "User does not exist")
+
+    user = authenticate(req, username=username, password=password)
+
+    if user is not None:
+      login(req, user)
+      return redirect('home')
+    else:
+      messages.error(req, "username or password does not exist.")
+      
+  context = {}
+  return render(req, 'base/login_register.html', context)
+
+def logoutUser(req):
+  logout(req)
+  return redirect('home')
 
 def home(req):
     
     q = req.GET.get('q') if req.GET.get('q') != None else ""
-    rooms = Room.objects.filter(topic__name__icontains = q)
+    
+    rooms = Room.objects.filter(
+      Q(topic__name__icontains = q) |
+      Q(name__icontains = q) |
+      Q(description__icontains = q)
+
+    )
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'count': room_count}
     return render(req, 'base/home.html', context)
 
 def room(req, pk):
